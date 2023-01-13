6d6a6316baa7bebb87a3c36b089fa890e076e066
Alkesh6 <tripathialkesh6@gmail.com>
2023-01-13 16:37:27 +0530

    Recursion1

--- src/recursion/questions.java
+++ src/recursion/questions.java
@@ -3,14 +3,30 @@ package recursion;
 public class questions {
     public static void main(String[] args) {
         questions q =  new questions();
-        var result = q.sum(12);
-        System.out.println(result);
+        // var result1 = q.sum(12);
+        // System.out.println(result1);
+
+        var result2 = q.power(2, 1);
+        System.out.println(result2);
     }
 
-    public int sum(int n){
-        if (n == 0 || n < 0){
-            return 0;
+    // Question 1
+    // public int sum(int n){
+    //     if (n == 0 || n < 0){
+    //         return 0;
+    //     }
+    //     return n%10 + sum(n/10);
+    // }
+
+
+    // Question 2
+    public int power(int base,int exp){
+        if(exp == 0){
+            return 1;
         }
-        return n%10 + sum(n/10);
+        else if(exp == 1){
+            return base;
+        }
+        return base * power(base,exp-1);
     }
 }
